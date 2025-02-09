93d134dba8afa92e37ede43397a660b788e11cf0
Philipe Fransozi <philipehf@gmail.com>
2025-02-09 09:25:19 -0300

    add aula02

--- sketch.js
+++ sketch.js
@@ -9,4 +9,14 @@ function draw() {
   circle(200,200,300);
   circle(150,150,60);
   circle(250, 150, 60);
+  
+  //line(x1, y1, x2, y2)
+  line(150, 270, 250, 270)
+
+  //triangle(x1, y1, x2, y2, x3, y3)
+  triangle(200, 180, 170, 220, 220, 220)
+
+  if (mouseIsPressed){
+    console.log(mouseX, mouseY)
+  }
 }
